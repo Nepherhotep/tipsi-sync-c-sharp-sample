9f1c781cdcd18f94895b2a73d1d8bdb8ff325ee2
zlov <zlov001@mail.ru>
2016-05-29 15:30:18 +0300

    Implemented "Sync" and "SyncClear". Removed Data classes.

--- TipsiSyncCSharp/TipsiSyncCSharp/Program.cs
+++ TipsiSyncCSharp/TipsiSyncCSharp/Program.cs
@@ -10,8 +10,10 @@
 namespace TipsiSyncCSharp
 {
     using System;
+    using System.Collections.Generic;
 
     using TipsiSyncCSharpClient;
+    using TipsiSyncCSharpClient.Models;
 
     /// <summary>
     /// The program.
@@ -24,17 +26,64 @@ namespace TipsiSyncCSharp
         /// <param name="args">The command line arguments.</param>
         static void Main(string[] args)
         {
-            TipsiClient tipsiClient = new TipsiClient("YOUR_LOGIN", "YOUR_PASSWORD");
+            // Prepare data
+            string login = "bottles-cases-api";
+            string password = "3bYtXvmTBZXX";
+            string apiVersion = "v001";
+            string storeID = "19351";
+            string baseAddress = "https://test.gettipsi.com";
+            List<Dictionary<string, object>> syncData = new List<Dictionary<string, object>>
+                                                            {
+                                                                new Dictionary<string, object>
+                                                                    {
+                                                                        { "barcode", "bar-525" },
+                                                                        { "bottle_size", 750 },
+                                                                        { "price", 34.4 },
+                                                                        { "in_stock", 12 }
+                                                                    },
+                                                                new Dictionary<string, object>
+                                                                    {
+                                                                        { "barcode", "bar-504" },
+                                                                        { "bottle_size", 750 },
+                                                                        { "price", 34.4 },
+                                                                        { "in_stock", 12 }
+                                                                    }
+                                                            };
+            
+            TipsiClient tipsiClient = new TipsiClient(baseAddress, apiVersion, login, password);
             tipsiClient.LoginAsync().Wait();
             Console.WriteLine("Login is successful!!!");
 
             // now you can sync
-            //client.Sync(batchJson).Wait();
+            SyncResult syncResult = tipsiClient.SyncAsync(storeID, syncData).Result;
+            PrintSyncResult(syncResult);
 
             // or
-            //client.SyncClear(batchJson).Wait();
+            syncResult = tipsiClient.SyncClearAsync(storeID, syncData).Result;
+            PrintSyncResult(syncResult);
+        }
 
-            Console.ReadKey();
+        /// <summary>
+        /// Print sync result at console.
+        /// </summary>
+        /// <param name="syncResult">The sync result.</param>
+        private static void PrintSyncResult(SyncResult syncResult)
+        {
+            Console.WriteLine();
+            if (syncResult.CreatedItemsCount != null)
+            {
+                Console.WriteLine("Created items count: {0}", syncResult.CreatedItemsCount);
+            }
+
+            if (syncResult.ClearedItemsCount != null)
+            {
+                Console.WriteLine("Cleared items count: {0}", syncResult.ClearedItemsCount);
+            }
+
+            if (syncResult.UpdatedItemsCount != null)
+            {
+                Console.WriteLine("Updated items count: {0}", syncResult.UpdatedItemsCount);
+            }
         }
     }
 }
---
+++ TipsiSyncCSharp/TipsiSyncCSharpClient/Models/SyncResult.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SyncResult.cs" company="Nepherhotep">
+//   Nepherhotep
+// </copyright>
+// <summary>
+//   The sync result.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TipsiSyncCSharpClient.Models
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The sync result.
+    /// </summary>
+    public class SyncResult
+    {
+        /// <summary>
+        /// Gets or sets the created items count.
+        /// </summary>
+        [JsonProperty("created_items")]
+        public int? CreatedItemsCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cleared items count.
+        /// </summary>
+        [JsonProperty("cleared_items")]
+        public int? ClearedItemsCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the updated items count.
+        /// </summary>
+        [JsonProperty("updated_items")]
+        public int? UpdatedItemsCount { get; set; }
+    }
+}
