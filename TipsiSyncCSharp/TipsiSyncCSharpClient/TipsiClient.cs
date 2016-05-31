3c098774f5503e26e1f0ffa6b4ec8cf52a3840f3
Alexey Zankevich <zancudero@gmail.com>
2016-05-31 11:32:03 +0300

    Updated tipsi client

--- TipsiSyncCSharp/TipsiSyncCSharpClient/TipsiClient.cs
+++ TipsiSyncCSharp/TipsiSyncCSharpClient/TipsiClient.cs
@@ -10,13 +10,16 @@
 namespace TipsiSyncCSharpClient
 {
     using System;
+    using System.Collections.Generic;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Text;
     using System.Threading.Tasks;
 
+    using Newtonsoft.Json;
+
     using TipsiSyncCSharpClient.Models;
-    using TipsiSyncCSharpClient.Settings;
+    using TipsiSyncCSharpClient.Utilities;
 
     /// <summary>
     /// The Tipsi http client.
@@ -26,7 +29,37 @@ namespace TipsiSyncCSharpClient
         /// <summary>
         /// The application/json media type.
         /// </summary>
-        public const string ApplicationJSONMediaType = "application/json";
+        private const string ApplicationJSONMediaType = "application/json";
+
+        /// <summary>
+        /// The login route pattern.
+        /// {0} - is version.
+        /// </summary>
+        private const string LoginRoutePattern = "/api/rest/{0}/login";
+
+        /// <summary>
+        /// The sync route pattern.
+        /// {0} - is version.
+        /// {1} - is store id.
+        /// </summary>
+        private const string SyncRoutePattern = "/api/rest/{0}/store/{1}/barcode/sync";
+        
+        /// <summary>
+        /// The sync clear route pattern.
+        /// {0} - is version.
+        /// {1} - is store id.
+        /// </summary>
+        private const string SyncClearRoutePattern = "api/rest/{0}/store/{1}/barcode/sync_clear";
+
+        /// <summary>
+        /// The version.
+        /// </summary>
+        private readonly string _version;
+
+        /// <summary>
+        /// The login route.
+        /// </summary>
+        private readonly string _loginRoute;
 
         /// <summary>
         /// The standart http client.
@@ -39,38 +72,85 @@ namespace TipsiSyncCSharpClient
         private readonly UserCredentials _userCredentials;
 
         /// <summary>
+        /// Checks resopnse for errors.
+        /// </summary>
+        /// <param name="response">The response.</param>
+        /// <returns>The <see cref="Task"/>. The responce content.</returns>
+        /// <exception cref="Exception"></exception>
+        private async Task<string> CheckResopnse(HttpResponseMessage response)
+        {
+            string responceContent = await response.Content.ReadAsStringAsync();
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(responceContent, ex);
+            }
+
+            return responceContent;
+        }
+
+        /// <summary>
         /// Logins at the service.
         /// </summary>
         /// <returns>The <see cref="Task"/>.</returns>
         public async Task LoginAsync()
         {
-            HttpResponseMessage result = await _httpClient.PostAsync(
-                Address.Default.LoginURL,
-                new StringContent(
-                    _userCredentials.ToJSON(),
-                    Encoding.UTF8,
-                    ApplicationJSONMediaType));
+            HttpResponseMessage responce = await _httpClient.PostAsync(
+                    _loginRoute,
+                    new StringContent(_userCredentials.ToJSON(), Encoding.UTF8, ApplicationJSONMediaType));
 
-            result.EnsureSuccessStatusCode();
-        }
-        
-        public void Sync(string batchJson)
-        {
+            await CheckResopnse(responce);
         }
 
-        public void SyncClear(string batchJson)
+        /// <summary>
+        /// Synchronizes the data asynchronously.
+        /// </summary>
+        /// <param name="storeId">The store ID.</param>
+        /// <param name="syncData">The wine Inventory For Syncs.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
+        public async Task<SyncResult> SyncAsync(string storeId, List<Dictionary<string, object>> syncData)
         {
+            HttpResponseMessage response = await _httpClient.PatchAsync(
+                    string.Format(SyncRoutePattern, _version, storeId),
+                    new StringContent(JsonConvert.SerializeObject(syncData), Encoding.UTF8, ApplicationJSONMediaType));
+
+            string responceContent = await CheckResopnse(response);
+            return JsonConvert.DeserializeObject<SyncResult>(responceContent);
+        }
+
+        /// <summary>
+        /// Clears items, except the given.
+        /// </summary>
+        /// <param name="storeId">The store ID.</param>
+        /// <param name="syncData">The sync data.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
+        public async Task<SyncResult> SyncClearAsync(string storeId, List<Dictionary<string, object>> syncData)
+        {
+            HttpResponseMessage response = await _httpClient.PatchAsync(
+                    string.Format(SyncClearRoutePattern, _version, storeId),
+                    new StringContent(JsonConvert.SerializeObject(syncData), Encoding.UTF8, ApplicationJSONMediaType));
+
+            string responceContent = await CheckResopnse(response);
+            return JsonConvert.DeserializeObject<SyncResult>(responceContent);
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TipsiClient"/> class.
         /// </summary>
+        /// <param name="baseAddress">The protocol and domain name (f.e. https://integration.gettipsi.com)</param>
+        /// <param name="version">The API version. (f.e. "v001").</param>
         /// <param name="login">The login.</param>
         /// <param name="password">The password.</param>
-        public TipsiClient(string login, string password)
+        public TipsiClient(string baseAddress, string version, string login, string password)
         {
+            _version = version;
+            _loginRoute = string.Format(LoginRoutePattern, version);
+
             // Create and configure standart http client.
-            _httpClient = new HttpClient { BaseAddress = new Uri(Address.Default.DomainURL) };
+            _httpClient = new HttpClient { BaseAddress = new Uri(baseAddress.TrimEnd('/')) };
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(ApplicationJSONMediaType));
 
---
+++ TipsiSyncCSharp/TipsiSyncCSharpClient/Utilities/HttpUtilities.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HttpUtilities.cs" company="Nepherhotep">
+//   Nepherhotep
+// </copyright>
+// <summary>
+//   Defines the HttpUtilities type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TipsiSyncCSharpClient.Utilities
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The http utilities and extensions.
+    /// </summary>
+    public static class HttpUtilities
+    {
+        /// <summary>
+        /// Sends http request with PATCH method.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        /// <param name="requestUri">The request uri.</param>
+        /// <param name="content">The content.</param>
+        /// <returns>The <see cref="Task"/>.</returns>
+        public static async Task<HttpResponseMessage> PatchAsync(
+            this HttpClient client,
+            string requestUri,
+            HttpContent content)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("PATCH"), requestUri)
+                                             {
+                                                 Content = content
+                                             };
+
+            return await client.SendAsync(request);
+        }
+    }
+}
